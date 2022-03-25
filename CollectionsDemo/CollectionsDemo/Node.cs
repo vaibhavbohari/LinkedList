f861364effd44d19115f8c021c0bd2cc5c70d0a6
vaibhavbohari <vaibhavbohari@gmail.com>
2022-03-25 17:27:18 +0530

    Adding Elements

---
+++ CollectionsDemo/CollectionsDemo/Node.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionsDemo
+{
+    internal class Node<Gtype>
+    {
+        public Gtype val;
+        public Node<Gtype> next;
+
+        public Node(Gtype data)
+        {
+            this.val = data;
+            this.next = null;
+
+        }
+    }
+}
--- CollectionsDemo/CollectionsDemo/Program.cs
+++ CollectionsDemo/CollectionsDemo/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Welcome to Data Structure");
+﻿using CollectionsDemo;
+{
+    Linked_List<int> mylist = new Linked_List<int>();
+    {
+        mylist.Add(20);
+        mylist.Add(30);
+        mylist.Add(40);
+        mylist.Add(50);
+        mylist.Add(60);
+        mylist.Add(70);
+        mylist.Add(80);
+        mylist.Display();
+    }
+}
